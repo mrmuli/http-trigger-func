375e7861a14881bfb5aba29a2983ad7fbde2fe66
mrmuli <jayjaymuli00@gmail.com>
2020-01-20 11:51:53 +0100

    feat: silence output

--- scripts/aci.sh
+++ scripts/aci.sh
@@ -6,10 +6,10 @@ resource_group="redemption-song"
 
 {
     # direct invocation
-    # az container create --resource-group $resource_group --name functions-demo --image josephmuli/http-trigger-func:1.0 --ip-address public --no-wait
+    az container create --resource-group $resource_group --name functions-demo --image josephmuli/http-trigger-func:1.0 --ip-address public --no-wait
     
     # create via a template file
-    az group deployment create --resource-group $resource_group --template-file aci-template.json --no-wait
-}
+    # az group deployment create --resource-group $resource_group --template-file aci-template.json --no-wait
+} &> /dev/null
 
 echo "done"
